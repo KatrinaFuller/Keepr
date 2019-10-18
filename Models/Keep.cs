f9ecbf660af5fa792b6b88a78d99868150761a00
Katrina Fuller <trinafuller@att.net>
2019-10-18 15:24:58 -0600

    added userId to Keep model

--- Models/Keep.cs
+++ Models/Keep.cs
@@ -5,6 +5,7 @@ namespace Keepr.Models
   public class Keep : IKeep
   {
     public int Id { get; set; }
+    public string UserId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Img { get; set; }
--- Repositories/KeepsRepository.cs
+++ Repositories/KeepsRepository.cs
@@ -1,36 +1,47 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
 using Keepr.Models;
 
 namespace Keepr.Repositories
 {
   public class KeepsRepository
   {
-    internal object Get()
+    private readonly IDbConnection _db;
+    public KeepsRepository(IDbConnection db)
+    {
+      _db = db;
+    }
+    public IEnumerable<Keep> Get()
+    {
+      string sql = "SELECT * FROM keeps";
+      return _db.Query<Keep>(sql);
+    }
+
+    public Keep GetKeepByUser(User user)
     {
       throw new NotImplementedException();
     }
 
-    internal Keep GetKeepByUser(User user)
+    public Keep GetKeepByKeepId(int id)
+    {
+      string sql = "SELECT * FROM keeps WHERE id = @id";
+      return _db.QueryFirstOrDefault<Keep>(sql, new { id });
+    }
+
+    public int Create(Keep newKeep)
+    {
+      string sql = @"
+      "
+    }
+
+    public void Edit(Keep keep)
     {
       throw new NotImplementedException();
     }
 
-    internal Keep GetKeepByKeepId(int id)
-    {
-      throw new NotImplementedException();
-    }
-
-    internal int Create(Keep newKeep)
-    {
-      throw new NotImplementedException();
-    }
-
-    internal void Edit(Keep keep)
-    {
-      throw new NotImplementedException();
-    }
-
-    internal void Delete(int id)
+    public void Delete(int id)
     {
       throw new NotImplementedException();
     }
