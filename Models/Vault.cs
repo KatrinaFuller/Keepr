13983e8d0b8e164e499adc7f222fa0a33f22da8c
Katrina Fuller <trinafuller@att.net>
2019-10-18 22:34:18 -0600

    created Vault model

--- Models/Vault.cs
+++ Models/Vault.cs
@@ -1,7 +1,12 @@
+using Keepr.Interfaces;
+
 namespace Keepr.Models
 {
-  public class Vault
+  public class Vault : IVault
   {
-
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
   }
 }
