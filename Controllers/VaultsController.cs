358cdea67dc2c7a5182f3b915477fefddcaa0830
Katrina Fuller <trinafuller@att.net>
2019-10-18 22:49:42 -0600

    filled out VaultsController

--- Controllers/VaultsController.cs
+++ Controllers/VaultsController.cs
@@ -1,7 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Keepr.Controllers
 {
-    public class VaultsController
+  [ApiController]
+  [Route("api/[controller")]
+  public class VaultsController : ControllerBase
+  {
+    private readonly VaultsService _vs;
+    public VaultsController(VaultsService vs)
     {
-        
+      _vs = vs;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Vault>> Get()
+    {
+      try
+      {
+        return Ok(_vs.Get());
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet]
+    public ActionResult<Vault> GetVaultByUserId()
+    {
+      try
+      {
+        var id = HttpContext.User.FindFirstValue("Id");
+        return Ok(_vs.GetVaultByUserId(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Vault> GetVaultById(int id)
+    {
+      try
+      {
+        return Ok(_vs.GetVaultById(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult<Vault> CreateVault([FromBody] Vault newVault)
+    {
+      try
+      {
+        newVault.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_vs.CreateVault(newVault));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete]
+    public ActionResult<int> DeleteVault(int id)
+    {
+      try
+      {
+        return Ok(_vs.DeleteVault(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+  }
 }
--- Services/VaultsService.cs
+++ Services/VaultsService.cs
@@ -1,7 +1,32 @@
+using System;
+using Keepr.Models;
+
 namespace Keepr.Services
 {
-    public class VaultsService
+  public class VaultsService
+  {
+    internal object Get()
     {
-        
+      throw new NotImplementedException();
     }
+    internal object GetVaultByUserId(string id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object GetVaultById(int id)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object CreateVault(Vault newVault)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object DeleteVault(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
