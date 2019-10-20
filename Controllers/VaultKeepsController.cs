58a160ba07a6f6224e9bb870e94380863bac546b
Katrina Fuller <trinafuller@att.net>
2019-10-19 21:52:54 -0600

    filled out VaultKeepsController

--- Controllers/VaultKeepsController.cs
+++ Controllers/VaultKeepsController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Security.Claims;
+using Keepr.Models;
 using Keepr.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,7 +8,7 @@ namespace Keepr.Controllers
 {
   [ApiController]
   [Route("api/[controller]")]
-  public class VaultKeepsController
+  public class VaultKeepsController : ControllerBase
   {
     private readonly VaultKeepsService _vks;
     public VaultKeepsController(VaultKeepsService vks)
@@ -13,7 +16,45 @@ namespace Keepr.Controllers
       _vks = vks;
     }
 
+    [HttpGet("{id}")]
+    public ActionResult<VaultKeep> GetVaultKeepsByVaultId(int id)
+    {
+      try
+      {
+        return Ok(_vks.GetVaultKeepsByVaultId(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
 
+    [HttpPost]
+    public ActionResult<VaultKeep> CreateVaultKeep([FromBody] VaultKeep newVaultKeep)
+    {
+      try
+      {
+        newVaultKeep.UserId = HttpContext.User.FindFirstValue("Id");
+        return Ok(_vks.CreateVaultKeep(newVaultKeep));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult<int> DeleteVaultKeep(int id)
+    {
+      try
+      {
+        return Ok(_vks.DeleteVaultKeep(id));
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
 
   }
 }
--- Services/VaultKeepsService.cs
+++ Services/VaultKeepsService.cs
@@ -1,7 +1,23 @@
+using System;
+using Keepr.Models;
+
 namespace Keepr.Services
 {
-    public class VaultKeepsService
+  public class VaultKeepsService
+  {
+    internal object GetVaultKeepsByVaultId(int id)
     {
-        
+      throw new NotImplementedException();
     }
+
+    internal object CreateVaultKeep(VaultKeep newVaultKeep)
+    {
+      throw new NotImplementedException();
+    }
+
+    internal object DeleteVaultKeep(int id)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
