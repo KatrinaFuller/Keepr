281ffb66c2bbd763fb1ddc3bc5fe5530978f21f1
Katrina Fuller <trinafuller@att.net>
2019-10-19 22:05:17 -0600

    filled out vaultKeepsRepository

--- Repositories/VaultKeepsRepository.cs
+++ Repositories/VaultKeepsRepository.cs
@@ -1,23 +1,38 @@
 using System;
+using System.Data;
+using Dapper;
 using Keepr.Models;
 
 namespace Keepr.Repositories
 {
   public class VaultKeepsRepository
   {
-    internal VaultKeep GetVaultKeepsByVaultId(int id)
+    private readonly IDbConnection _db;
+    public VaultKeepsRepository(IDbConnection db)
     {
-      throw new NotImplementedException();
+      _db = db;
     }
 
-    internal int CreateVaultKeep(VaultKeep newVaultKeep)
+    public VaultKeep GetVaultKeepsByVaultId(int id)
     {
-      throw new NotImplementedException();
+      string sql = "SELECT * FROM vaultkeeps WHERE id = @id";
+      return _db.QueryFirstOrDefault<VaultKeep>(sql, new { id });
     }
 
-    internal void DeleteVaultKeep(int id)
+    public int CreateVaultKeep(VaultKeep newVaultKeep)
     {
-      throw new NotImplementedException();
+      string sql = @"
+      INSERT INTO vaultkeeps
+      (vaultId, userId, keepId)
+      VALUES
+      (@VaultId, @UserId, @KeepId)";
+      return _db.ExecuteScalar<int>(sql, newVaultKeep);
+    }
+
+    public void DeleteVaultKeep(int id)
+    {
+      string sql = "DELETE FROM vaultkeeps WHERE id = @id";
+      _db.Execute(sql, new { id });
     }
   }
 }
