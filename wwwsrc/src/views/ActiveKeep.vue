1700a2b1dc632801a8ab7ac6e06faa79620ff811
Katrina Fuller <trinafuller@att.net>
2019-10-20 18:58:50 -0600

    starting ActiveKeep

---
+++ wwwsrc/src/views/ActiveKeep.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col">
+        <div class="activeKeep"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "activeKeep",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
