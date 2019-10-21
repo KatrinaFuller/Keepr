6480c8e8299684ddf7b8f69e2d135eed6f56373b
Katrina Fuller <trinafuller@att.net>
2019-10-21 13:08:31 -0600

    starting to make addVaultModal

---
+++ wwwsrc/src/components/addVaultModal.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="add-vault-modal"></div>
+</template>
+
+
+<script>
+export default {
+  name: "add-vault-modal",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
