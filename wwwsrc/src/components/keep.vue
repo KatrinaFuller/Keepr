0bbaad900b6fd432805ea5c79779f00353eca2bb
Katrina Fuller <trinafuller@att.net>
2019-10-20 18:40:18 -0600

    added script in html & trying to make keep card

--- wwwsrc/src/components/keep.vue
+++ wwwsrc/src/components/keep.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="keep row">
-    <div class="col-3">keep</div>
+    <div class="col-3">
+      <div class="card" style="width: 18rem;">
+        <!-- <img class="card-img-top" v-bind:src="`${keep.Img}`" alt="keep image" /> -->
+        <div class="card-body">
+          <p class="card-text">{{Keep.Description}}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
