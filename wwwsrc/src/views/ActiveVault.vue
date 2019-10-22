95f9b41445956ccac2a0850cf1c3c9c6fcb040e0
Katrina Fuller <trinafuller@att.net>
2019-10-21 21:16:17 -0600

    made file for activeVault

---
+++ wwwsrc/src/views/ActiveVault.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="activeVault"></div>
+</template>
+
+
+<script>
+export default {
+  name: "activeVault",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+
+
+<style scoped>
+</style>
