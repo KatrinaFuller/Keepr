62f24bf4fb33b9e28179006e3df7df196f53887c
Katrina Fuller <trinafuller@att.net>
2020-02-18 11:24:56 -0700

    adding Vue Material

--- wwwsrc/src/main.js
+++ wwwsrc/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import { MdButton, MdCard } from 'vue-material/dist/components'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import AuthService from "./AuthService"
+
+Vue.use(MdButton)
+Vue.use(MdCard)
 
 //Vue.config.productionTip = false
 
