acdfedb43c9eb7791345a6daa72fe5abea895ce9
Katrina Fuller <trinafuller@att.net>
2020-02-18 19:27:07 -0700

    changing where the add keep/vault modals are

--- wwwsrc/src/views/Profile.vue
+++ wwwsrc/src/views/Profile.vue
@@ -4,30 +4,17 @@
       <profileNavbar />
       <div class="row">
         <addKeepModal />
-        <addVaultModal />
-        <!-- add button -->
-        <button class="btn m-1" type="button" data-toggle="collapse" data-target="#collapseAdd">
+        <button class="btn btn-circle m-1" data-toggle="modal" data-target="#add-keep-modal">
           <i class="fas fa-plus"></i>
         </button>
       </div>
-      <div class="row">
-        <!-- add keep or add vaults -->
-        <div class="collapse" id="collapseAdd">
-          <button class="btn m-1" data-toggle="modal" data-target="#add-keep-modal">Add Keep</button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="collapse" id="collapseAdd">
-          <button class="btn m-1" data-toggle="modal" data-target="#add-vault-modal">Add Vault</button>
-        </div>
-      </div>
+
       <div class="row">
         <div class="col-12">
           <h3>Keeps</h3>
         </div>
         <keep v-for="keep in keeps" :key="keep.id" :keepProp="keep" />
       </div>
-      <div class="row"></div>
     </div>
   </div>
 </template>
@@ -37,7 +24,6 @@
 import router from "@/router.js";
 import profileNavbar from "../components/profileNavbar";
 import addKeepModal from "../components/addKeepModal";
-import addVaultModal from "../components/addVaultModal";
 import keep from "../components/keep";
 import vaultsList from "../components/vaultsList";
 import vault from "../components/vault";
@@ -67,7 +53,6 @@ export default {
   components: {
     profileNavbar,
     addKeepModal,
-    addVaultModal,
     keep,
     vault,
     vaultsList
@@ -80,6 +65,9 @@ export default {
 .bg {
   background-color: #f5f5f5;
   min-height: 100vh;
+}
+.btn-circle {
+  border-radius: 50%;
 }
 h3 {
   color: #2c2f33;
--- wwwsrc/src/views/Vaults.vue
+++ wwwsrc/src/views/Vaults.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid bg">
     <div class="vaults">
       <vaultsNavbar />
-      <div class="col-12">
-        <h3>Vaults</h3>
+      <div class="row">
+        <addVaultModal />
+        <button class="btn btn-circle m-1" data-toggle="modal" data-target="#add-vault-modal">
+          <i class="fas fa-plus"></i>
+        </button>
       </div>
-      <vault v-for="vault in vaults" :key="vault.vaults" :vaultProp="vault" />
+      <div class="row">
+        <div class="col-12">
+          <h3>Vaults</h3>
+        </div>
+        <vault v-for="vault in vaults" :key="vault.vaults" :vaultProp="vault" />
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +25,7 @@ import keep from "../components/keep";
 import vaultsList from "../components/vaultsList";
 import vault from "../components/vault";
 import vaultsNavbar from "../components/vaultsNavbar";
+import addVaultModal from "../components/addVaultModal";
 
 export default {
   name: "vaults",
@@ -36,7 +45,8 @@ export default {
     keep,
     vault,
     vaultsList,
-    vaultsNavbar
+    vaultsNavbar,
+    addVaultModal
   }
 };
 </script>
@@ -46,6 +56,9 @@ export default {
 .bg {
   background-color: #f5f5f5;
   min-height: 100vh;
+}
+.btn-circle {
+  border-radius: 50%;
 }
 h3 {
   color: #2c2f33;
